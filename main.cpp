75ac865d61a58e6e0719b84f5786e78a4d28ab96
Ivan Gomez <ivan.gquintero@alumnos.udg.mx>
2020-09-28 17:40:24 -0500

    Se completo el codigo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
 using namespace std;
-
+//char int short float double long
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+
+    cout << "Dame el tipo de dato " <<endl;
+    getline(cin, cadena);
+
+    if(cadena == "int"){
+        cout << sizeof(int) << endl;
+    }else if(cadena == "char"){
+        cout << sizeof(char) << endl;
+    }else if(cadena == "short"){
+        cout << sizeof(short) << endl;
+    }else if(cadena == "float"){
+        cout << sizeof(float) << endl;
+    }else if(cadena == "double"){
+        cout << sizeof(double) << endl;
+    }else if(cadena == "long"){
+        cout << sizeof(long) << endl;
+    }
+
 
     return 0;
 }
